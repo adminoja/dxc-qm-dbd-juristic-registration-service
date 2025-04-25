8317635be7458dc1054c7ecb774b80f8e54a6726
Pichada Sungthong <pichada@clearskysolutions.co.th>
2025-04-25 15:12:31 +0700

    add application.yml

---
+++ src/main/java/th/go/dxc/DxcQmDbdJuristicRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package th.go.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxcQmDbdJuristicRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxcQmDbdJuristicRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/th/go/dxc/DxcQmDbdJuristicRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package th.go.dxc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxcQmDbdJuristicRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
