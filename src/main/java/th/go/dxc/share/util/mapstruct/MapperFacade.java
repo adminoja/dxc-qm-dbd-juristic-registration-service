23cb12150826a4188d941cb9304214f0d14258ea
kittivong <kittivong@gmail.com>
2026-01-30 11:27:25 +0700

    update pom sql server to 13.2.1-jre11 fix DQDJR-003

--- src/main/java/th/go/dxc/share/util/mapstruct/MapperFacade.java
+++ src/main/java/th/go/dxc/share/util/mapstruct/MapperFacade.java
@@ -6,6 +6,6 @@ import th.go.dxc.app.model.DbdJuristicRegistration;
 import th.go.dxc.infra.connector_juristic_api.model.response.JuristicRegistrationResponse;
 
 @Mapper(componentModel = "spring")
-public interface MapperFacade {
+public interface MapperFacade {   
     public DbdJuristicRegistration toDbdJuristicRegistration(JuristicRegistrationResponse model);
 }
